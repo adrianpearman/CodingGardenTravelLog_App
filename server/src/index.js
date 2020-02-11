66acf9c77c93871277de278bf26eb8b7bd12abfd
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-11 12:15:08 -0500

    added helmet and morgan to the application.

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(morgan("common"));
+app.use(helmet());
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
