bb86c5dd4fa07b23e94f96fcdb8b8e4afbb16591
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-12 01:04:03 -0500

    moving front end code to repo.

---
+++ frontend/build/precache-manifest.279cb9be5d251d4c7018b1c83bd10502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bb9ee45f47802c5ce731de420ac516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72189b81e198aa4ae6c4",
+    "url": "/static/css/main.7d5efe07.chunk.css"
+  },
+  {
+    "revision": "7e5fcfc9770c9bd1880b",
+    "url": "/static/js/2.cbce5601.chunk.js"
+  },
+  {
+    "revision": "f19081fd6d75179b8e3102db46a64814",
+    "url": "/static/js/2.cbce5601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72189b81e198aa4ae6c4",
+    "url": "/static/js/main.ec4ea1f6.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
---
+++ frontend/src/components/LogEntryForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { createLogEntry } from "../util/api";
+
+const LogEntryForm = ({ latitude, longitude, onClose }) => {
+  const { register, handleSubmit } = useForm();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const onSubmit = async data => {
+    try {
+      setLoading(true);
+      await createLogEntry({ ...data, latitude, longitude });
+      onClose();
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form className="entryForm" onSubmit={handleSubmit(onSubmit)}>
+      {error ? <h3 className="error">{error}</h3> : null}
+      <label htmlFor="title">Title</label>
+      <input name="title" required ref={register} />
+      <label htmlFor="comments">Comments</label>
+      <textarea name="comments" row={3} ref={register}></textarea>
+      <label htmlFor="description">Description</label>
+      <textarea name="description" row={3} ref={register}></textarea>
+      <label htmlFor="image">Image</label>
+      <input name="image" ref={register} />
+      <label htmlFor="visitDate">Visit Date</label>
+      <input name="visitDate" type="date" required ref={register} />
+      <button disabled={loading}>
+        {loading ? "Loading..." : "Create Log Entry"}
+      </button>
+    </form>
+  );
+};
+
+export default LogEntryForm;
