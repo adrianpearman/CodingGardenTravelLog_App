35df42fc095b3c09c77c5941c72923c9bebfd84c
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-12 02:04:18 -0500

    linted backend.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 // Variables
 const app = express();
-const middlewares = require("./middlewares/middlewares");
-const monogoConnection = require("./middlewares/mongoConnection");
+const clientRoot = require('path').join(__dirname, '../client', 'build');
+const middlewares = require('./middlewares/middlewares');
+const monogoConnection = require('./middlewares/mongoConnection');
 
 const PORT = process.env.PORT || 3001;
 
 // Database
 monogoConnection();
 
-//Client Root Path
-const clientRoot = require("path").join(__dirname, "../client", "build");
+// Client Root Path
 
 // Middlewares
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Render client root buildfile if the application is ona production server
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(clientRoot));
-  app.get("/", (req, res) => {
-    res.sendFile("index.html", { clientRoot });
+  app.get('/', (req, res) => {
+    res.sendFile('index.html', { clientRoot });
   });
 }
-app.use("/api/logs", require("./apiRoutes/logs"));
+app.use('/api/logs', require('./apiRoutes/logs'));
 
 app.use(middlewares.unfoundRoute);
 app.use(middlewares.generalErrorHandler);
