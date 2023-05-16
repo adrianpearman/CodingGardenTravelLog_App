8baea4848fd99af301c576dc2f63dbae427c57b8
Adrian Pearman <adrianpearman12@gmail.com>
2023-05-16 19:32:53 -0400

    refactored config folder

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
-} else {
-  module.exports = require("./dev");
-}
+module.exports = {
+  MONGOURI: process.env.MONGOURI,
+  PORT: process.env.PORT,
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+  LOG_APIKEY: process.env.LOG_APIKEY,
+};
--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGOURI: process.env.MONGOURI,
-  PORT: process.env.PORT,
-  CORS_ORIGIN: process.env.CORS_ORIGIN,
-  LOG_APIKEY: process.env.LOG_APIKEY
-};
